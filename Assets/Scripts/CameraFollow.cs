d20b1215c19e59588cf41e133ad6d927e1b39d9b
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-08-02 10:25:54 -0700

    Added smooth camera movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        Vector3 goalPos = target.position;
+        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+    }
+}
+
