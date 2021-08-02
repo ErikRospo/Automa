60d49abc015e2eed5f13333671300ea0ea42b1f3
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-08-02 00:38:56 -0700

    Added temp sand background (WIP)

---
+++ Assets/Scripts/WorldGen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WorldGen : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Transform sandTile;
+
+    public void Start()
+    {
+
+        //for (int a = -100; a <= 100; a++)
+        //    for (int b = -100; b <= 100; b++)
+                
+    }
+
+    private void CreateTile()
+    {
+        
+    }
+}
