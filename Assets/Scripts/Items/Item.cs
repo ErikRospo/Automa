d9d81d60dad156aaf286ab6d47ef4ef56fe93407
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2021-08-04 00:23:16 -0400

    Basic Item ScriptableObjects

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Items/Item")]
+public class Item : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public int maxStackSize;
+    public Sprite icon;
+
+    [System.Serializable]
+    public struct Recipe
+    {
+        public string name;
+        public Item item;
+        public int amount;
+    }
+    public Recipe[] recipe;
+}
