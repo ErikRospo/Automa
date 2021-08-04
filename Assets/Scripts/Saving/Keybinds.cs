d1cc3a76d19cf19cfa100ac32487f978ba60737a
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-08-03 22:10:50 -0700

    Started keybind support

---
+++ Assets/Scripts/Saving/Keybinds.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keybinds
+{
+    public static string action_move_up = "w";
+    public static string action_move_down = "s";
+    public static string action_move_left = "a";
+    public static string action_move_right = "d";
+    public static string action_sprint = "left shift";
+    public static string action_shoot = "mouse 0";
+    public static string action_equip = "e";
+    public static string action_hotbar_1 = "1";
+    public static string action_hotbar_2 = "2";
+    public static string action_hotbar_3 = "3";
+    public static string action_hotbar_4 = "4";
+    public static string action_hotbar_5 = "5";
+    public static string action_hotbar_6 = "6";
+    public static string action_hotbar_7 = "7";
+    public static string action_hotbar_8 = "8";
+    public static string action_hotbar_9 = "9";
+    public static string action_hotbar_0 = "0";
+}
