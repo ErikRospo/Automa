c305a473182ef46c2a2c2beb49ec4b511e022475
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-08-04 19:57:18 -0700

    Create inventory slot script

---
+++ Assets/Scripts/Player/InventorySlot.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Inventory.InventoryItem inventoryItem;
+    public Image icon;
+
+    private void Start()
+    {
+        icon = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void SetItem(Inventory.InventoryItem inventoryItem)
+    {
+        this.inventoryItem = inventoryItem;
+        icon.sprite = Resources.Load<Sprite>("Icons/" + inventoryItem.item.name);
+    }
+
+    public Inventory.InventoryItem TakeItem()
+    {
+        Inventory.InventoryItem holder = inventoryItem;
+        RemoveItem();
+        return holder;
+    }
+
+    public void RemoveItem()
+    {
+        inventoryItem = new Inventory.InventoryItem();
+        icon.sprite = Resources.Load<Sprite>("Icons/Empty");
+    }
+}
