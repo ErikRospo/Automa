42bfa202c590905fe03507914bccf969df19804e
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-10-17 08:20:47 -0700

    Add Fizzy Steamworks and Heathens

---
+++ Assets/_Heathen Engineering/Assets/Steamworks/Scripts/Foundation/Common/SteamworksCreator.cs
@@ -0,0 +1,28 @@
+﻿#if !DISABLESTEAMWORKS && !UNITY_SERVER
+using UnityEngine;
+
+namespace HeathenEngineering.SteamAPI
+{
+    [HelpURL("https://kb.heathenengineering.com/assets/steamworks")]
+    [DisallowMultipleComponent]
+    public class SteamworksCreator : MonoBehaviour
+    {
+        public bool createOnStart;
+        public bool markAsDoNotDestroy;
+        public SteamSettings settings;
+
+        private void Start()
+        {
+            if (createOnStart)
+                CreateIfMissing(settings, markAsDoNotDestroy);
+        }
+
+        public void CreateIfMissing()
+        {
+            CreateIfMissing(settings, markAsDoNotDestroy);
+        }
+
+        public static void CreateIfMissing(SteamSettings settings, bool doNotDestroy = false) => settings.CreateBehaviour(doNotDestroy);
+    }
+}
+#endif
