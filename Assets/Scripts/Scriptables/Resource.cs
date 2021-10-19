e1bbb61ae86c56ba4aebcfcfcdac5b45f14d60d5
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-10-19 09:49:11 -0700

    Add biome and natural asset support for world generation

--- Assets/Scripts/Scriptables/Resource.cs
+++ Assets/Scripts/Scriptables/Resource.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Raw Iron", menuName = "World/Raw Iron")]
+[CreateAssetMenu(fileName = "New Resource", menuName = "World/Resource")]
 public class Resource : ScriptableObject
 {
-    public new string name;
-    public Item item;
+    public new string name; // Name of the resource
+    public Item item; // Item that will be given when mined
+    public bool usePerlinNoise; // Tells generator if perlin noise should be used
+    public bool randomizeRotation; // Tells generator if resource can be rotated
     public float spawnScale; // Scale of perlin noise
     public float spawnThreshold; // Perlin float threshold
 }
--- Assets/Scripts/World/WorldGen.cs
+++ Assets/Scripts/World/WorldGen.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 using UnityEngine;
 
 public class WorldGen : MonoBehaviour
@@ -10,8 +11,11 @@ public class WorldGen : MonoBehaviour
     public ResourceTile resourceTile;
     public List<Resource> resources;
     public float offset = 0;
+    public float perlinScale = 500;
 
     // Resource grid
+    public Tilemap biomeTextureMap;
+    [HideInInspector] public Grid biomeGrid;
     [HideInInspector] public Grid resourceGrid;
 
     // Loaded chunks
@@ -30,7 +34,7 @@ public class WorldGen : MonoBehaviour
         Random.InitState(Random.Range(0, 1000000000));
         if (offset == 0) offset = Random.Range(0, 10000);
     }
-
+    
     // Generate resources
     public void UpdateChunks(Vector2Int chunk)
     {
@@ -43,49 +47,96 @@ public class WorldGen : MonoBehaviour
             // Check that the chunk isn't loaded
             if (loadedChunks.Contains(newChunk)) continue;
 
-            // Loop through resources
-            foreach (Resource resource in resources)
-            {
-                // Get world coordinate
-                int xValue = newChunk.x * 20;
-                int yValue = newChunk.y * 20;
+            // A CHECK NEEDS TO BE DONE HERE
+            // If chunk is brand new, generate biome / resources
+            // If chunk is not new, grab it from the object pool 
+            // If chunk is outside of player view, pool and disable it
 
-                for (int x = xValue; x < xValue + 100; x++)
+            // Update biome
+            //UpdateBiome(newChunk);
+            GenerateResources(newChunk);
+        }
+
+        // Update loaded chunks
+        loadedChunks = new List<Vector2Int>(chunks);
+    }
+
+    // Loops through a chunk and updates the biome if required
+    private void UpdateBiome(Vector2Int newChunk)
+    {
+
+    }
+
+    // Loops through a new chunk and spawns resources based on perlin noise values
+    private void GenerateResources(Vector2Int newChunk)
+    {
+        // Get world coordinate
+        int xValue = newChunk.x * 20;
+        int yValue = newChunk.y * 20;
+
+        // Loop through x and y coordinates
+        for (int x = xValue; x < xValue + 100; x++)
+        {
+            for (int y = yValue; y < yValue + 100; y++)
+            {
+                // Loop through resources
+                foreach (Resource resource in resources)
                 {
-                    for (int y = yValue; y < yValue + 100; y++)
+                    // ANOTHER CHECK NEEDS TO BE DONE HERE
+                    // - Check if resource being spawned is able to reside in biome
+                    // - If not, skip. If so, spawn it.
+
+                    // Check if resource uses perlin noise
+                    if (!resource.usePerlinNoise)
+                    {
+                        // Calculate random float value
+                        float value = Random.value;
+                        if (resource.spawnThreshold < value)
+                        {
+                            // If threshold exceed value, spawn
+                            TrySpawnResource(resource, x, y);
+                            break;
+                        }
+                    }
+                    else
                     {
                         // Calculate perlin noise pixel
-                        float xCoord = ((float)x / 100 * resource.spawnScale) + offset;
-                        float yCoord = ((float)y / 100 * resource.spawnScale) + offset;
+                        float xCoord = ((float)x / perlinScale) + offset;
+                        float yCoord = ((float)y / perlinScale) + offset;
                         float value = Mathf.PerlinNoise(xCoord, yCoord);
 
                         // If value exceeds threshold, try and generate
                         if (value >= resource.spawnThreshold)
                         {
-                            // Get x and y pos
-                            float xPos = (x * 5f) - 250;
-                            float yPos = (y * 5f) - 250;
-
-                            // Check cell to make sure it's empty
-                            if (resourceGrid.RetrieveCell(Vector2Int.RoundToInt(new Vector2(xPos, yPos))) == null)
-                            {
-                                // Create the resource
-                                ResourceTile temp = Instantiate(resourceTile.gameObject, new Vector3(xPos, yPos, -1), Quaternion.identity).GetComponent<ResourceTile>();
-                                resourceGrid.SetCell(Vector2Int.RoundToInt(temp.transform.position), true);
-
-                                // Set resource values
-                                temp.name = resource.name;
-                                temp.resource = resource;
-                                temp.spriteRenderer.sprite = SpritesManager.GetSprite(resource.name);
-                                temp.transform.position = new Vector3(temp.transform.position.x, temp.transform.position.y, 0f);
-                            }
+                            TrySpawnResource(resource, x, y);
+                            break;
                         }
                     }
                 }
             }
         }
+    }
 
-        loadedChunks = new List<Vector2Int>(chunks);
+    // Try and spawn a resource
+    private void TrySpawnResource(Resource resource, int x, int y)
+    {
+        // Get x and y pos
+        float xPos = (x * 5f) - 250;
+        float yPos = (y * 5f) - 250;
+
+        // Check cell to make sure it's empty
+        if (resourceGrid.RetrieveCell(Vector2Int.RoundToInt(new Vector2(xPos, yPos))) == null)
+        {
+            // Create the resource
+            ResourceTile temp = Instantiate(resourceTile.gameObject, new Vector3(xPos, yPos, -1), Quaternion.identity).GetComponent<ResourceTile>();
+            resourceGrid.SetCell(Vector2Int.RoundToInt(temp.transform.position), true);
+
+            // Set resource values
+            temp.name = resource.name;
+            temp.resource = resource;
+            temp.spriteRenderer.sprite = SpritesManager.GetSprite(resource.name);
+            temp.transform.position = new Vector3(temp.transform.position.x, temp.transform.position.y, 0f);
+        }
     }
 
     private List<Vector2Int> GetChunks(Vector2Int chunk)
