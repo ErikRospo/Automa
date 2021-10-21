03aabd0236a0cccf1a1c315f3f094bbe9179fc49
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2021-10-21 15:00:21 -0400

    Use empty GameObjects instead of  multiple canvases

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
 using TMPro;
+using HeathenEngineering.SteamAPI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Canvases
-    [SerializeField] private Canvas mainCanvas;
-    [SerializeField] private Canvas hostCanvas;
-    [SerializeField] private Canvas joinCanvas;
+    // Screens
+    [SerializeField] private GameObject mainScreen;
+    [SerializeField] private GameObject hostScreen;
+    [SerializeField] private GameObject joinScreen;
 
     // Network Manager
     [SerializeField] private NetworkManager networkManager;
@@ -21,25 +22,26 @@ public class MainMenu : MonoBehaviour
     // Switch canvas to main screen
     public void GoToMainCanvas()
     {
-        mainCanvas.gameObject.SetActive(true);
-        hostCanvas.gameObject.SetActive(false);
-        joinCanvas.gameObject.SetActive(false);
+        mainScreen.SetActive(true);
+        hostScreen.SetActive(false);
+        joinScreen.SetActive(false);
     }
 
     // Switch canvas to host screen
     public void GoToHostCanvas()
     {
-        mainCanvas.gameObject.SetActive(false);
-        hostCanvas.gameObject.SetActive(true);
-        joinCanvas.gameObject.SetActive(false);
+        mainScreen.SetActive(false);
+        hostScreen.SetActive(true);
+        joinScreen.SetActive(false);
     }
 
     // Switch canvas to join screen
     public void GoToJoinCanvas()
     {
-        mainCanvas.gameObject.SetActive(false);
-        hostCanvas.gameObject.SetActive(false);
-        joinCanvas.gameObject.SetActive(true);
+        mainScreen.SetActive(false);
+        hostScreen.SetActive(false);
+        joinScreen.SetActive(true);
+        UpdateFriendsList();
     }
 
     public void Host()
@@ -59,5 +61,13 @@ public class MainMenu : MonoBehaviour
     public void UpdateMaxPlayers()
     {
         maxPlayersSlider.GetComponentInChildren<TextMeshProUGUI>().text = $"Max Players ({(int) maxPlayersSlider.value})";
+    }
+
+    public void UpdateFriendsList()
+    {
+        foreach (UserData friend in SteamSettings.Client.ListFriends())
+        {
+            Debug.Log(friend.DisplayName);
+        }
     }
 }
