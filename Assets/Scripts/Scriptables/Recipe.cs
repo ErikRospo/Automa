bb4977742941672c171c115690eab3eac3a69765
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-10-20 19:20:28 -0700

    Add copper wire

--- Assets/Scripts/Scriptables/Recipe.cs
+++ Assets/Scripts/Scriptables/Recipe.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Recipe", menuName = "Recipe")]
+[CreateAssetMenu(fileName = "New Recipe", menuName = "Items/Recipe")]
 public class Recipe : ScriptableObject
 {
     public new string name;
     public RecipeItem[] input;
     public RecipeItem[] output;
     public float time;
-    public bool unlocked;
+    public bool enabled;
 }
