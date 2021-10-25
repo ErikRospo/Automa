d136cc78869b97d1e7c275c59ad7e7ebb8afc360
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-10-25 16:15:00 -0700

    Fix rotational checks on non-square buildings

--- Assets/Scripts/Handlers/BuildingHandler.cs
+++ Assets/Scripts/Handlers/BuildingHandler.cs
@@ -31,7 +31,7 @@ public class BuildingHandler : NetworkBehaviour
         if (tile == null) return;
 
         // Check to make sure the tiles are not being used
-        if (!CheckTiles(tile, position)) return;
+        if (!CheckTiles(tile, position, rotation)) return;
 
         // Instantiate the object like usual
         RpcInstantiateBuilding(tile, position, rotation, option);
@@ -52,34 +52,13 @@ public class BuildingHandler : NetworkBehaviour
         // Apply options if required
         if (option != -1) lastBuilding.ApplyOptions(option);
 
-        // Create variables for grid cycle
-        Vector2 cellPos;
-
         // Set the tiles on the grid class
         if (tile.cells.Length > 0)
         {
             foreach (BuildingTile.Cell cell in tile.cells)
             {
-                // Determine rotational changes
-                switch (rotation.eulerAngles.z)
-                {
-                    case 90f:
-                        cellPos = new Vector2(lastBuilding.transform.position.x - cell.y, 
-                                              lastBuilding.transform.position.y + cell.x);
-                        break;
-                    case 180f:
-                        cellPos = new Vector2(lastBuilding.transform.position.x - cell.x, 
-                                              lastBuilding.transform.position.y - cell.y);
-                        break;
-                    case 270f:
-                        cellPos = new Vector2(lastBuilding.transform.position.x + cell.y, 
-                                              lastBuilding.transform.position.y - cell.x);
-                        break;
-                    default:
-                        cellPos = new Vector2(lastBuilding.transform.position.x + cell.x, 
-                                              lastBuilding.transform.position.y + cell.y);
-                        break;
-                }
+                // Set cells based on rotation
+                Vector2 cellPos = RotateVector(lastBuilding.transform.position, new Vector2(cell.x, cell.y), rotation);
                 tileGrid.SetCell(Vector2Int.RoundToInt(cellPos), true, tile, lastBuilding);
 
                 // If debug mode enabled, display coordinates
@@ -92,6 +71,7 @@ public class BuildingHandler : NetworkBehaviour
         }
         else
         {
+            // Set cell based on transform position
             tileGrid.SetCell(Vector2Int.RoundToInt(lastBuilding.transform.position), true, tile, lastBuilding);
 
             if (debugMode)
@@ -100,6 +80,28 @@ public class BuildingHandler : NetworkBehaviour
                 holder.position = new Vector3(holder.position.x, holder.position.y, holder.position.z - 1.5f);
             }
         }
+    }
+
+    public Vector2 RotateVector(Vector2 original, Vector2 cell, Quaternion rotation)
+    {
+        // Determine rotational changes
+        Vector2 cellPos;
+        switch (rotation.eulerAngles.z)
+        {
+            case 90f:
+                cellPos = new Vector2(original.x - cell.y, original.y + cell.x);
+                break;
+            case 180f:
+                cellPos = new Vector2(original.x - cell.x, original.y - cell.y);
+                break;
+            case 270f:
+                cellPos = new Vector2(original.x + cell.y, original.y - cell.x);
+                break;
+            default:
+                cellPos = new Vector2(original.x + cell.x, original.y + cell.y);
+                break;
+        }
+        return cellPos;
     }
 
     // Destroys a building
@@ -111,7 +113,7 @@ public class BuildingHandler : NetworkBehaviour
 
     // Checks to make sure tile(s) isn't occupied
     [Server]
-    public bool CheckTiles(BuildingTile tile, Vector3 position)
+    public bool CheckTiles(BuildingTile tile, Vector3 position, Quaternion rotation)
     {
         // Tells system to check tile placement
         bool checkTilePlacement = tile.spawnableOn.Count > 0;
@@ -121,7 +123,9 @@ public class BuildingHandler : NetworkBehaviour
             foreach (BuildingTile.Cell cell in tile.cells)
             {
                 // Check to make sure nothing occupying tile
-                Vector2Int coords = Vector2Int.RoundToInt(new Vector2(position.x + cell.x, position.y + cell.y));
+                Vector2Int coords = Vector2Int.RoundToInt(RotateVector(position, new Vector2(cell.x, cell.y), rotation));
+
+                // Attempt to retrieve the cell
                 if (tileGrid.RetrieveCell(coords) != null)
                     return false;
 
