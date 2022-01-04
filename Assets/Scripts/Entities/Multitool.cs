f20bc47131364d17a323236db37e3cf39b9b8554
Vitzual <65577955+Vitzual@users.noreply.github.com>
2022-01-04 13:03:33 -0800

    Add first rendition of the drop pod

--- Assets/Scripts/Entities/Multitool.cs
+++ Assets/Scripts/Entities/Multitool.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class Multitool : Equipment
 {
     // Animator component
-    
+    public Animator animator;
 
     // Use method
     public override void UseEquipment()
     {
+        switch(setting)
+        {
+            // Ping tool
+            case 0:
 
+                break;
+
+            // Drill tool
+            case 1:
+
+                break;
+
+            // Scan tool
+            case 2:
+
+                break;
+
+            // Flash tool
+            case 3:
+
+                break;
+        }
     }
 }
