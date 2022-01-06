e63dad1b4d10cfb64e2159bcf4ede69fde69749e
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2022-01-06 12:05:10 -0500

    Document Stat Class

--- Assets/Scripts/Data/Stat.cs
+++ Assets/Scripts/Data/Stat.cs
@@ -1,6 +1,15 @@
+/// <summary>
+/// Data-holder for statistics that have a minimum, maximum, and current value
+/// </summary>
 public class Stat
 {
-    // Constructor for new stats
+    /// <summary>
+    /// Create a full <see cref="Stat"/> class. All params must be NonNull
+    /// </summary>
+    /// <param name="type">The <see cref="Stat.Type"/> of this satistic.</param>
+    /// <param name="min">The minimum <see cref="float"/> of this statstic.</param>
+    /// <param name="current">The starting <see cref="float"/> of this statistic</param>
+    /// <param name="max"></param>
     public Stat(Type type, float min, float current, float max)
     {
         this.type = type;
@@ -9,7 +18,9 @@ public class Stat
         this.max = max;
     }
 
-    // Stat types
+    /// <summary>
+    /// The types of <see cref="Stat"/>s that are possible
+    /// </summary>
     public enum Type
     {
         Health,
@@ -22,21 +33,43 @@ public class Stat
         Thirst
     }
 
-    // Stat values
+    /// <summary>
+    /// The <see cref="Stat.Type"/> of this <see cref="Stat"/>
+    /// </summary>
     public Type type;
+
+    /// <summary>
+    /// The minimum value of this <see cref="Stat"/>.
+    /// </summary>
     public float min;
+
+    /// <summary>
+    /// The current value of this <see cref="Stat"/>.
+    /// </summary>
     public float current;
+
+    /// <summary>
+    /// The maximum value of this <see cref="Stat"/>.
+    /// </summary>
     public float max;
 
     /// <summary>
     /// Gets normalized percentage
     /// </summary>
-    /// <returns>float between 0 and 1</returns>
+    /// <returns><see cref="float"/> between 0 and 1</returns>
     public float GetPercentage() {
         return (current - min) / (max - min);
     }
 
-    // Check if at max or min
+    /// <summary>
+    /// Checks if this <see cref="Stat"/> is at the maximum value.
+    /// </summary>
+    /// <returns>True if this <see cref="Stat"/> is at the maximum.</returns>
     public bool IsAtMax() { return current >= max; }
+
+    /// <summary>
+    /// Checks if this <see cref="Stat"/> is at the minimum value.
+    /// </summary>
+    /// <returns>True if this <see cref="Stat"/> is at the minimum value.</returns>
     public bool IsAtMin() { return current <= min; }
 }
